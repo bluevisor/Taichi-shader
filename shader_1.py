290707a57bca172860bd3f2bd7c58ee37cc18402
John Zheng <bluevisor@gmail.com>
2023-12-06 06:51:55 +0300

    Update shader_1.py

--- shader_1.py
+++ shader_1.py
@@ -7,14 +7,17 @@ ti.init(arch=ti.gpu)  # Initialize Taichi with GPU (if available)
 width, height = 1728, 984
 pixels = ti.Vector.field(4, dtype=float, shape=(width, height))
 
-speed = 0.01
-complexity = 4
-scale = 2 # bigger number = zoomed out
-fragment = 1.5 #bigger number = smaller pieces 
-repeat = 4
-smallCircleCenter = 0.5
-bigCircleCenter = {'x': 0.5, 'y': 0.5}
-glow = 0.005
+speed = 0.01 # 0.01
+complexity = 4 # 4
+scale = 2 # 2 bigger number = zoomed out
+fragment = 1.5 # 1.5 bigger number = smaller pieces 
+repeat = 4 # 4
+smallCircleCenter = 0.5 # 0.5
+bigCircleCenter = {'x': 0.5, 'y': 0.5} # 0.5, 0.5
+glow = 0.01 # 0.01
+glowPow = 1.2 # 1.2
+pPow = 0.4 # 0.4
+tPow = 0.2 # 0.2
 
 @ti.func
 def fract(x):
@@ -39,10 +42,10 @@ def paint(t: float):
         for p in range(complexity): # loop for small circles
             uv = fract(uv * fragment) - smallCircleCenter # small circles
             d = uv.norm() * ti.exp(-uv0.norm())  # big circle
-            color = palette(uv0.norm() + p * 0.4 + t * 0.2)  # color gradient + time shift
+            color = palette(uv0.norm() + p * pPow + t * tPow)  # color gradient + time shift
             d = ti.sin(d * repeat + t) / repeat  # sin wave repetition
             d = ti.abs(d)  # negative numbers are black, this makes the inside bright
-            d = ti.pow(glow / d, 1.2) # glow / brightness
+            d = ti.pow(glow / d, glowPow) # glow / brightness
 
             finalColor += color * d
 
